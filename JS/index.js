f0f5ad9350849004dc830bb131b0d6c5ba9a40f1
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-29 15:47:19 -0400

    added new API and finished logic

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,7 @@
 $(function() {
 fetchWord()
 
-// defining elements
-let wordChallenge = $('h3').text()
-// end
-
+let newWord
 // hit "enter" function
 $(document).keypress(test)
 
@@ -13,15 +10,16 @@ function test(event) {
   if (event.keyCode === 13) { // if pressed enter
     let wordInput = $('input').val()
     console.log(wordInput)
-    console.log(wordChallenge)
+    console.log(newWord)
     evaluateInput(wordInput)
+    fetchWord()
   }
 }
 // end
 
 // evaluate input
 function evaluateInput(wordInput) {
-  if(wordInput === wordChallenge) {
+  if(wordInput === newWord) {
     console.log('correct!')
   } else {
     console.log('you fucked')
@@ -30,14 +28,34 @@ function evaluateInput(wordInput) {
 // end
 
 // pull phrase from API
+// async function fetchWord() {
+//   try {
+//     const url = "https://api.adviceslip.com/advice"
+//
+//     const response = await axios.get(url)
+//     console.log(response)
+//     console.log(response.data.slip.advice)
+//     updateUi(response.data.slip.advice)
+//     clearInput()
+//     // function goes here to update UI
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// end
+
+
+// MTG API
+// https://api.magicthegathering.io/v1/cards
 async function fetchWord() {
   try {
-    const url = "https://api.adviceslip.com/advice"
+    const url = "https://api.magicthegathering.io/v1/cards"
 
     const response = await axios.get(url)
-    console.log(response)
-    console.log(response.data.slip.advice)
-    updateUi(response.data.slip.advice)
+    let list = response.data.cards
+    let index = [Math.floor(Math.random() * response.data.cards.length)];
+    newWord = list[index].name
+    updateUi(newWord)
     clearInput()
     // function goes here to update UI
   } catch (error) {
@@ -54,7 +72,7 @@ function updateUi(newWord){
 
 // clear input box
 function clearInput() {
-  $('input').html(' ')
+  $('input').val('')
 }
 // end
 
