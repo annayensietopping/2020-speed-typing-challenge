be283e39b19f08329ee85c33e2ab1fa7df61d99a
oona baby <oonababy@noahs-mbp-2.lan>
2020-03-31 19:26:09 -0400

    i think timing was fixed?

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,8 @@ $(function() {
 let newWord
 let time
 let score
-let isPlaying
+let isPlaying = true
+let stopwatch
 
 fetchWord()
 setTime()
@@ -21,7 +22,9 @@ function test(event) {
     console.log(newWord)
     evaluateInput(wordInput)
     fetchWord()
-    let stopwatch = setInterval(countdown, 1000)
+
+    stopwatch = setInterval(countdown, 1000)
+    setInterval(checkStatus,1000)
   }
 }
 // end
@@ -31,74 +34,14 @@ function evaluateInput(wordInput) {
   if(wordInput === newWord) {
     addPoint()
     console.log('correct!')
-    time = 5
+    time = 6
   } else {
-    setScore()
     console.log('not a match')
+    $('h2').text("Not a Match")
     searchGif("no")
+    playAgain()
+    isPlaying = false
   }
-}
-// end
-
-// pull phrase from API
-// async function fetchWord() {
-//   try {
-//     const url = "https://api.adviceslip.com/advice"
-//
-//     const response = await axios.get(url)
-//     console.log(response)
-//     console.log(response.data.slip.advice)
-//     updateUi(response.data.slip.advice)
-//     clearInput()
-//     // function goes here to update UI
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-// end
-
-
-// MTG API
-// https://api.magicthegathering.io/v1/cards
-async function fetchWord() {
-  try {
-    const url = "https://api.magicthegathering.io/v1/cards"
-
-    const response = await axios.get(url)
-    let list = response.data.cards
-    let index = [Math.floor(Math.random() * response.data.cards.length)];
-    newWord = list[index].name
-    updateUi(newWord)
-    clearInput()
-    // function goes here to update UI
-  } catch (error) {
-    console.log(error)
-  }
-}
-// end
-
-// update UI with new phrase
-function updateUi(newWord){
-  $('h3').text(newWord)
-}
-//end
-
-// clear input box
-function clearInput() {
-  $('input').val('')
-}
-// end
-
-// set time
-function setTime() {
-  time = 5
-  $('.time').html(time)
-}
-// end
-
-// set time
-function updateTime() {
-  $('.time').html(time)
 }
 // end
 
@@ -109,21 +52,51 @@ function countdown() {
     console.log(time)
     updateTime(time)
   } else if (time === 0) {
-    console.log('gameover')
-    playAgain()
-searchGif("time's up")
-isPlaying = false
+    // console.log('gameover')
+    // $('h2').text("Oops too slow")
+    // playAgain()
+    // searchGif("gameover")
+    isPlaying = false
   }
 }
 // end
 
 // check status
 function checkStatus() {
-  if (!isPlaying && time === 0)
+  if (isPlaying === false) {
+  stopTime()
+} else if (time === 0) {
+  console.log('gameover')
+  $('h2').text("Oops too slow")
+  playAgain()
+  searchGif("gameover")
+}}
+//
+
+function stopTime() {
   clearInterval(stopwatch)
 }
 
-//
+// clear input box
+function clearInput() {
+  $('input').val('')
+}
+// end
+
+// set time
+function setTime() {
+  time = 5
+  updateTime(time)
+}
+// end
+
+// set time
+function updateTime() {
+  $('.time').html(time)
+}
+// end
+
+
 
 // set score
 function setScore() {
@@ -179,5 +152,31 @@ function displayResults(response) {
       $('#reaction').html(`<img src="${response[n].images.original.url}"/>`)
   }
 //
+
+// MTG API
+// https://api.magicthegathering.io/v1/cards
+async function fetchWord() {
+  try {
+    const url = "https://api.magicthegathering.io/v1/cards"
+
+    const response = await axios.get(url)
+    let list = response.data.cards
+    let index = [Math.floor(Math.random() * response.data.cards.length)];
+    newWord = list[index].name
+    updateUi(newWord)
+    clearInput()
+    // function goes here to update UI
+  } catch (error) {
+    console.log(error)
+  }
+}
+// end
+
+// update UI with new phrase
+function updateUi(newWord){
+  $('h3').text(newWord)
+}
+//end
+
 
 })
